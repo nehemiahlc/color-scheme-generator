ea0108b817266206309f82a346b0d0434ffba25e
nehemiah <nehemiahl.chandler@gmail.com>
2025-06-05 04:27:04 -0500

    Added initial files

---
+++ script.js
@@ -0,0 +1,78 @@
+const colorPicker = document.getElementById('color-picker')
+const lightDarkModeBtn = document.getElementById('modeBtn')
+const colorSchemeBtn = document.getElementById('btn-colors')
+const colorTypeInput = document.getElementById('color-type')
+const colorLabels = document.getElementById('color-hexes')
+const colorsContainers = document.getElementById('colors-container')
+
+let chosenColorArray = []
+
+
+lightDarkModeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode')
+   }
+)
+
+colorSchemeBtn.addEventListener('click', () => handleClick())
+
+colorLabels.addEventListener('click', e => {
+        const value = e.target.closest('[id]')
+        navigator.clipboard.writeText(value.textContent)
+        alert('Copied the text: ' + value.textContent)
+})
+
+colorsContainers.addEventListener('click', e => {
+    const value = e.target.closest('[id]')
+    navigator.clipboard.writeText(value.textContent)
+    alert('Copied the text: ' + value.textContent)
+})
+
+
+
+
+
+function handleClick() {
+    const color = colorPicker.value
+    const mode = colorTypeInput.value
+    fetchData(color, mode)
+}
+
+
+
+
+function fetchData(color, mode) {
+
+    colorLabels.innerHTML = ''
+    colorsContainers.innerHTML = ''
+    chosenColorArray = []
+    
+    let chosenColor = color.slice(1)
+    let chosenMode = mode.toLowerCase()
+    
+    fetch(`https://www.thecolorapi.com/scheme?hex=${chosenColor}&mode=${chosenMode}`)
+        .then(res => res.json())
+        .then(data => {
+            // let html = ``
+            for (let i = 0; i < data.colors.length; i++){
+                // html += `<p id=${i}>${data.colors[i].hex.value}</p>`
+                chosenColorArray.push(data.colors[i].hex.value)
+
+            }
+            let id = 0
+            chosenColorArray.map(color => {
+                id++
+                let newDiv = document.createElement('div')
+                newDiv.style.backgroundColor = color
+                newDiv.classList.add('color')
+                newDiv.textContent = color
+                newDiv.id = id
+                document.getElementById('colors-container').appendChild(newDiv)
+            })
+            
+            // colorLabels.innerHTML = html
+        })  
+
+
+}
+
+  
